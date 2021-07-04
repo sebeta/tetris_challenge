64b590ce1cd8abc987f3c8c0a8e62b4e82b48aba
Sebastian <sebeta057@gmail.com>
2021-07-04 17:30:33 -0500

    Cleaning the code and adding comments

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-
+// Getting the container element with the squares:
 let container = document.querySelector('.container')
 
+// Assigning an even listener for clicks to the container and linking it with function changeColor().
+container.addEventListener('click', changeColor)
+
+// Creating a function to change the color according to the value (color) selected of the list.
+// Parameter 'e' corresponds to an event. This function is linked with an event listener.
 function changeColor(e) {
+    // Getting the value (color) selected in the list.
     let colorOption = document.getElementById('color_list').value
-    console.log(e.target.tagName)
-    if (e.target.tagName == 'DIV') {
-        e.target.style.backgroundColor = colorOption
+    
+    // 'e.target' is each element clicked inside the container element.
+    // Just want to change the color of 'div' elemtents (the squares).
+    if (e.target.tagName == 'DIV') { 
+        e.target.style.backgroundColor = colorOption 
     }
 }
-
-container.addEventListener('click', changeColor, true)
